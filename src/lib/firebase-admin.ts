e1fc5906d9e55e670e893fdd226c7ea18dde6942
publix store <publixstore.exe@gmail.com>
2025-07-27 07:18:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,20 +1,18 @@
-
 import admin from 'firebase-admin';
 import 'server-only';
 
 let adminApp: admin.app.App;
 
 if (!admin.apps.length) {
-  const privateKey = process.env.FIREBASE_PRIVATE_KEY;
-  if (!privateKey) {
-    throw new Error('The FIREBASE_PRIVATE_KEY environment variable was not found.');
-  }
-
   const serviceAccount: admin.ServiceAccount = {
     projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: privateKey.replace(/\\n/g, '\n'),
+    privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   };
+
+  if (!serviceAccount.projectId || !serviceAccount.privateKey || !serviceAccount.clientEmail) {
+    throw new Error('Firebase service account environment variables are not set correctly.');
+  }
 
   try {
     adminApp = admin.initializeApp({
